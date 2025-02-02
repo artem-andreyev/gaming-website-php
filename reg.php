adc2ed381e37df46eb8a2dac624b441ccc4b27eb
Artem <artem.andreyev212@gmail.com>
2025-02-02 12:08:07 +0200

    Working with dynamic php pages

--- reg.php
+++ reg.php
@@ -4,42 +4,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Game Website</title>
+    <title>Sign up</title>
     <link rel="stylesheet" href="/css/main.css">
 </head>
 
 <body>
     <?php require_once "blocks/header.php"; ?>
 
-    <div class="container hero-contacts">
-        <h1>Lorem Ipsum is simply dummy text of the printing and.</h1>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
-        <img src="/img/Map.png" alt="">
-    </div>
-
     <div class="feedback">
         <div class="container">
-            <h2>Say hello</h2>
+            <h2>Sign up</h2>
             <p>Lorem Ipsum is simply dummy text of the printing .</p>
 
             <form>
                 <div class="inline">
                     <div>
-                        <label>First Name</label>
+                        <label>Username</label>
                         <input type="text">
                     </div>
                     <div>
-                        <label>Last Name</label>
+                        <label>First Name</label>
                         <input type="text">
                     </div>
                 </div>
                 <label>Email Address</label>
                 <input type="email" class="one-line">
 
-                <label>Message</label>
-                <textarea class="one-line"></textarea>
+                <label>Password</label>
+                <input type="email" class="one-line">
 
-                <button type="button">Get in touch</button>
+                <button type="button">Sign up</button>
             </form>
         </div>
     </div>
